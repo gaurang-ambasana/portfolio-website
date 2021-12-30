6f970c45088de8cd8b063481b44dc11064121aad
Gaurang Ambasana <gpambasana@gmail.com>
2021-12-31 02:28:12 +0530

    routed everything to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Intro from "./components/intro/Intro";
 import About from "./components/about/About";
 import ProjectInfo from "./components/projects-info/ProjectInfo";
+import ContactInfo from "./components/contact-info/ContactInfo";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Intro />
       <About />
       <ProjectInfo />
+      <ContactInfo />
     </>
   );
 };
