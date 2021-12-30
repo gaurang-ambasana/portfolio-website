707a6c98cb03524ea41e99bdfb76e85a6bcc8e35
Gaurang Ambasana <gpambasana@gmail.com>
2021-12-30 17:36:33 +0530

    added about me comp.

---
+++ src/components/about/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./about.css";
+import aboutBg from "../../img/about-bg.jfif";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img src={aboutBg} alt="" className="a-img" />
+        </div>
+      </div>
+      <div className="a-right">
+        <h1 className="a-about">About Me</h1>
+        <p className="a-sub">??</p>
+        <p className="a-desc">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore
+          suscipit eveniet odio reiciendis alias quo laboriosam hic, nihil totam
+          incidunt autem nesciunt aliquam cupiditate aut, enim fuga consequuntur
+          labore quis.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
