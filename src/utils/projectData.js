f3e6b8a2fb5de95560d5b04c98688cafe9922ff2
Gaurang Ambasana <gpambasana@gmail.com>
2021-12-30 23:51:30 +0530

    added project information

---
+++ src/utils/projectData.js
@@ -0,0 +1,20 @@
+export const projectData = [
+  {
+    id: 1,
+    name: "Proshop 🛍",
+    img: "proshop.PNG",
+    link: "https://mern-ecommerce-webapp.herokuapp.com/",
+  },
+  {
+    id: 2,
+    name: "GitHub Finder 🤨",
+    img: "git-finder.PNG",
+    link: "https://github-finder-52.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Website Monitoring 🔭",
+    img: "website-monitoring.png",
+    link: "https://github.com/gaurang-ambasana/monior-changes-on-webpage",
+  },
+];
