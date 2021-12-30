d9fa21dff6b85fe643605adc997593dd94df661e
Gaurang Ambasana <gpambasana@gmail.com>
2021-12-30 23:52:14 +0530

    added project info module

---
+++ src/components/projects-info/ProjectInfo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./projectInfo.css";
+import Project from "../project/Project";
+import { projectData } from "../../utils/projectData";
+
+const ProjectInfo = () => {
+  return (
+    <div className="pl">
+      <h1 className="pl-title">Project Work</h1>
+      <div className="pl-list">
+        {projectData.map((project) => (
+          <Project
+            key={project.id}
+            name={project.name}
+            img={project.img}
+            link={project.link}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectInfo;
