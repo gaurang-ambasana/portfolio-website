d2f28235ca35d2a176d2dd9ce5f704a9aac6bc81
Gaurang Ambasana <gpambasana@gmail.com>
2021-12-31 02:29:21 +0530

    fixed typo

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -6,7 +6,7 @@ const Intro = () => {
     <div className="i">
       <div className="i-left">
         <div className="i-left-wrapper">
-          <h2 className="i-intro">Hello 👋 My name is</h2>
+          <h2 className="i-intro">Hello There 👋 My name is</h2>
           <h1 className="i-name">Gaurang 😎</h1>
           <div className="i-title">
             <div className="i-title-wrapper">
