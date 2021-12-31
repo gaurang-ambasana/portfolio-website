402c37d4ab11ea4786f989ce361b6ed9322dd8f6
Gaurang Ambasana <gpambasana@gmail.com>
2022-01-01 01:48:27 +0530

    added actual skill info

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,6 @@
 import "./about.css";
 import aboutBg from "../../img/about-bg.jfif";
+import { skillsLeft, skillsRight } from "./skills";
 
 const About = () => {
   return (
@@ -12,13 +13,31 @@ const About = () => {
       </div>
       <div className="a-right">
         <h1 className="a-about">About Me</h1>
-        <p className="a-sub">??</p>
+        <p className="a-sub">I'm an Aspiring Full Stack Developer</p>
+        <br />
         <p className="a-desc">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore
-          suscipit eveniet odio reiciendis alias quo laboriosam hic, nihil totam
-          incidunt autem nesciunt aliquam cupiditate aut, enim fuga consequuntur
-          labore quis.
+          Being a Software Engineer, you should believe in forever learning
+          principles. <br /> My area of expertise includes but not limited to
         </p>
+        <br />
+        <div className="skill-list">
+          <div className="s-left">
+            {skillsLeft.map(({ name, img }, id) => (
+              <div key={id} className="s-item">
+                <img src={img} alt={name} className="s-img" />
+                <p>{name}</p>
+              </div>
+            ))}
+          </div>
+          <div className="s-right">
+            {skillsRight.map(({ name, img }, id) => (
+              <div key={id} className="s-item">
+                <img src={img} alt={name.substring(0, 2)} className="s-img" />
+                <p>{name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
