cfa2b54a6a770d288d9913ba3657240083958639
Gaurang Ambasana <gpambasana@gmail.com>
2021-12-31 23:50:25 +0530

    added theme toggle button

---
+++ src/components/toggle/Toggle.jsx
@@ -0,0 +1,30 @@
+import "./toggle.css";
+import sunIcon from "../../img/sun.png";
+import moonIcon from "../../img/moon.png";
+import { useContext } from "react";
+import { ThemeContext } from "../../context/context";
+
+const Toggle = () => {
+  const { dispatch, state } = useContext(ThemeContext);
+  const { darkMode } = state;
+
+  const buttonStyle = {
+    left: darkMode ? 0 : 25,
+  };
+
+  const action = {
+    type: "TOGGLE",
+  };
+
+  const toggleTheme = () => dispatch(action);
+
+  return (
+    <div className="t">
+      <img className="t-icon" src={sunIcon} alt="sun" />
+      <img className="t-icon" src={moonIcon} alt="moon" />
+      <div className="t-btn" onClick={toggleTheme} style={buttonStyle}></div>
+    </div>
+  );
+};
+
+export default Toggle;
