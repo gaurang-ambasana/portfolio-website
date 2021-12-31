326e2c99aad1c2a1657fe79f2cb1a25a7fca3c3b
Gaurang Ambasana <gpambasana@gmail.com>
2022-01-01 01:48:56 +0530

    ofc! added skills

---
+++ src/components/about/skills.js
@@ -0,0 +1,37 @@
+export const skillsLeft = [
+  {
+    name: "JavaScript",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+  },
+  {
+    name: "Python",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+  },
+  {
+    name: "NodeJS",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+  },
+  {
+    name: "React",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+  },
+];
+
+export const skillsRight = [
+  {
+    name: "Express",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+  },
+  {
+    name: "MongoDB",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+  },
+  {
+    name: "Google Cloud",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original.svg",
+  },
+  {
+    name: "Google Apps Script",
+    img: "https://seeklogo.com/images/G/google-apps-script-logo-BDEAA5E2DF-seeklogo.com.png",
+  },
+];
