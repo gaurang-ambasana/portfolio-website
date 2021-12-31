4c0e62247c69ad7f356a6352c0c0ca8403e33226
Gaurang Ambasana <gpambasana@gmail.com>
2021-12-31 23:52:47 +0530

    wrapped with dark mode context

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import Intro from "./components/intro/Intro";
 import About from "./components/about/About";
 import ProjectInfo from "./components/projects-info/ProjectInfo";
 import ContactInfo from "./components/contact-info/ContactInfo";
+import Toggle from "./components/toggle/Toggle";
+import { useContext } from "react";
+import { ThemeContext } from "./context/context";
 
 const App = () => {
+  const { state } = useContext(ThemeContext);
+  const { darkMode } = state;
+
+  const themeBasedStyle = {
+    backgroundColor: darkMode ? "#222" : "yellow",
+    color: darkMode && "white",
+  };
+
   return (
-    <>
+    <div style={themeBasedStyle}>
+      <Toggle />
       <Intro />
       <About />
       <ProjectInfo />
       <ContactInfo />
-    </>
+    </div>
   );
 };
 
