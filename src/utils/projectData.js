67f22bedb3459c2509647c4f5ef049762141a9ed
Gaurang Ambasana <gpambasana@gmail.com>
2022-04-05 20:48:05 +0530

    added project info

--- src/utils/projectData.js
+++ src/utils/projectData.js
@@ -1,20 +1,32 @@
 export const projectData = [
   {
     id: 1,
-    name: "Proshop 🛍",
+    name: "Proshop",
     img: "proshop.PNG",
     link: "https://mern-ecommerce-webapp.herokuapp.com/",
   },
   {
     id: 2,
-    name: "GitHub Finder 🤨",
+    name: "GitHub Finder",
     img: "git-finder.PNG",
     link: "https://github-finder-52.netlify.app/",
   },
   {
     id: 3,
-    name: "Website Monitoring 🔭",
+    name: "Website Monitoring",
     img: "website-monitoring.png",
     link: "https://github.com/gaurang-ambasana/monior-changes-on-webpage",
+  },
+  {
+    id: 4,
+    name: "2048 Clone",
+    img: "2048-img.jpeg",
+    link: "https://clone-of-2048.herokuapp.com/",
+  },
+  {
+    id: 5,
+    name: "Mini Postman",
+    img: "postman-clone.png",
+    link: "https://github.com/gaurang-ambasana/api-tester-cli",
   },
 ];
